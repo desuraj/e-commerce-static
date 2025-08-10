cfff178c580eb6512e87fc86c8299c2b5076fadf
desuraj <suraj.deo1160@gmail.com>
2025-08-10 20:50:06 +0530

    minor change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // Replace with your repo name
 export default defineConfig({
-  base: '/e-commerce/',
+  base: '/e-commerce-static/',
   plugins: [react()],
 })
